d323c476afa59dc01aa0a8c3de832a5d54017005
mariiio <merdan-9@outlook.com>
2023-04-15 15:44:11 +0800

    feat: add zksync_era example

---
+++ hardhat.config.ts
@@ -0,0 +1,34 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// set proxy
+const proxyUrl = process.env.PROXY_URL;   // change to yours, With the global proxy enabled, change the proxyUrl to your own proxy link. The port may be different for each client.
+const { ProxyAgent, setGlobalDispatcher } = require("undici");
+const proxyAgent = new ProxyAgent(proxyUrl);
+setGlobalDispatcher(proxyAgent);
+
+// package
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+
+
+
+module.exports = {
+  zksolc: {
+    version: "1.3.8",
+    compilerSource: "binary",
+    settings: {},
+  },
+  defaultNetwork: "zkSyncTestnet",
+
+  networks: {
+    zkSyncTestnet: {
+      url: "https://testnet.era.zksync.dev",
+      ethNetwork: process.env.GOERLI_RPC_URL, // RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
+    },
+  },
+  solidity: {
+    version: "0.8.8",
+  },
+};
